9ad30eca40020c23c39df82a6e36e1b870ae20d4
Peter Chege <peterkagure@gmail.com>
2023-04-08 23:08:54 +0300

    Some refactors

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { createTRPCRouter } from "n/server/api/trpc";
-import { exampleRouter } from "n/server/api/routers/example";
+import {createTRPCRouter} from "n/server/api/trpc";
+import {exampleRouter} from "n/server/api/routers/example";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "n/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+     example: exampleRouter,
 });
 
 // export type definition of API
